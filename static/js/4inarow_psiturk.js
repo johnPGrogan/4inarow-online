32ae1c12f4b13fae4aceaa884502210dffd1a441
cogneuroAWS <john.grogan@ndcn.ox.ac.uk>
2020-10-09 15:47:47 +0000

    20 trials

--- static/js/4inarow_psiturk.js
+++ static/js/4inarow_psiturk.js
@@ -37,7 +37,7 @@ $(window).on('load', function() {
 	psiturk.recordUnstructuredData('start', String(new Date()));
 	return saveData()
 	.then(() => setTimeout(() => {
-		initialize_task(10);
+		initialize_task(20);
 		// PsiTurk considers a participant hasn't started until they have finished the instruction.
 		// We want psiTurk to treat them as started right away to get the refresh/page close protection.
 		psiturk.finishInstructions();
